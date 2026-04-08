e7e260fc2e288e0cebde14a2bb5b00e2908e27ce
anaaz1798 <ana.az1798@icloud.com>
2026-04-07 20:19:33 -0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://vjuyguksfehgqgmeujzj.supabase.co'
-const supabaseAnonKey = 'sb_publishable_eDpPsWHv2qVadhSYL_m3Zg_zoQNwRtS'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqdXlndWtzZmVoZ3FnbWV1anpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzNDQ1ODIsImV4cCI6MjA5MDkyMDU4Mn0.LsgHIQSDHpSPtLYpe8z75rrCO42TH0G2Gi-zsfe4c7I'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)

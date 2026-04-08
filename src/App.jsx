b0c84b859e7ac081ea3f30303aa067783b9ff9a5
anaaz1798 <ana.az1798@icloud.com>
2026-04-08 17:17:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import ServicesPage from './pages/ServicesPage';
 import Reservar from './pages/Reservar';
+import ServicesPage from './pages/ServicesPage';
 import EventosPage from './pages/EventosPage';
 import VitrinaPage from './pages/VitrinaPage';
-import Navbar from './components/Navbar'; // <--- IMPORTA EL NAVBAR AQUÍ
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      <div className="pb-20"> {/* Espacio para que el menú no tape el contenido */}
+      <div className="pb-20"> {/* Espacio para que el menú no tape nada */}
         <Routes>
+          {/* Esta es la pantalla de inicio con el botón rosa y banners */}
           <Route path="/" element={<LoginPage />} />
-          <Route path="/servicios" element={<ServicesPage />} />
+          
+          {/* Esta es la nueva página compacta que creaste */}
           <Route path="/reservar" element={<Reservar />} />
+          
+          {/* Otras rutas existentes */}
+          <Route path="/servicios" element={<ServicesPage />} />
           <Route path="/eventos" element={<EventosPage />} />
           <Route path="/vitrina" element={<VitrinaPage />} />
         </Routes>
-        <Navbar /> {/* <--- PONLO AQUÍ PARA QUE SE VEA SIEMPRE */}
+        
+        {/* El Navbar se mantiene abajo siempre */}
+        <Navbar />
       </div>
     </Router>
   );
